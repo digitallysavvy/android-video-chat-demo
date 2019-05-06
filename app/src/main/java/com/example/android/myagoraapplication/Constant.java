6306ffda29c527916a4bcf9838ac627c53408ab7
Hermes <hermes.frangoudis@gmail.com>
2019-05-06 12:48:40 -0400

    added constants file

---
+++ app/src/main/java/com/example/android/myagoraapplication/Constant.java
@@ -0,0 +1,12 @@
+package com.example.android.myagoraapplication;
+
+/**
+ * Created by digitallysavvy May 2019.
+ */
+
+public class Constant {
+    public static final int CALL_IN = 0x01;
+    public static final int CALL_OUT = 0x02;
+
+    private static long timeLast;
+}
